cc0c6f1569b8652fe7a120a55b730609fca15e4d
Santheesh-S <santheesh24@gmail.com>
2025-04-18 03:49:00 +0000

    Code challenge submission for question: 9 Palindrome Number

---
+++ 9 Palindrome Number/9palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        long rev=0;
+        if(x<0 || (x!=0 && x%10==0)) return false;
+        while(x>rev)
+        {
+            rev=rev*10+(x%10);
+            x/=10;
+        }
+        if(x==rev||x==rev/10)
+            return true;
+        return false;
+    }
+}
