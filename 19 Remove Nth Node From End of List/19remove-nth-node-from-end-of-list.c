67ea4ee9dd5aad47431e3e57cf89968d5048a884
Santheesh-S <santheesh24@gmail.com>
2025-04-25 14:52:12 +0000

    Code challenge submission for question: 19 Remove Nth Node From End of List

---
+++ 19 Remove Nth Node From End of List/19remove-nth-node-from-end-of-list.c
@@ -0,0 +1,33 @@
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+
+    int len = 0;
+    struct ListNode *temp = head;
+    while (temp != NULL) {
+        len++;
+        temp = temp->next;
+    }
+
+    if (n > len) {
+        return head; 
+    }
+
+    if (n == len) {
+        struct ListNode* newHead = head->next;
+        free(head);
+        return newHead;
+    }
+
+    // Find the node before the one we want to remove
+    int targetIndex = len - n;
+    temp = head;
+    for (int i = 1; i < targetIndex; i++) {
+        temp = temp->next;
+    }
+
+    // Remove the nth node from the end
+    struct ListNode* nodeToRemove = temp->next;
+    temp->next = nodeToRemove->next;
+    free(nodeToRemove);
+
+    return head;
+}
